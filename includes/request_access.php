b1c4d3b48225c9968b72bcfb4525c6053d15f56d
Katrina <32313004+brownkat6@users.noreply.github.com>
2020-10-17 12:58:50 -0400

    Create request_access.php

---
+++ includes/request_access.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+	$message = "You requested to trade!";
+?>
+
+<script>
+        $(function() {
+         $('#myModal').modal('show');
+        });
+        </script>
+
+    <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Edit Data</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="fetched-data"><?php $message ?></div> 
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
