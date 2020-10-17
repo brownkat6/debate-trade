9ef91736cb6980f29e52b2c5697e3a55d3f08a4a
Katrina <32313004+brownkat6@users.noreply.github.com>
2020-10-17 13:14:59 -0400

    Update cardtrade_respond.js

--- includes/cardtrade_respond.js
+++ includes/cardtrade_respond.js
@@ -1,36 +1,19 @@
+// Call some ajax to set the offerID in the PHP server-side $_SESSION['cardtrade_offerID']
+// once that is done, then refresh the 'respond to offer' ninja form, in a new window
 function cardtrade_setSessionOfferID(offerID) {
-	// check this out:  https://www.w3schools.com/js/js_ajax_intro.asp
-	// call some ajax to set the offerID in the PHP server-side $_SESSION['cardtrade_offerID']
-
-	// once that is done, then refresh the 'respond to offer' ninja form, like in a new window
-	//HttpContext.Current.Session["offerID"] = offerID;
   jQuery(document).ready(function($) {
 
 	$.ajax({
         type : 'POST',
-        url : ajax_object.ajax_url, //admin_url('admin-ajax.php') ,
-        //dataType: 'json',
+        url : ajax_object.ajax_url,
         data: {
 				action : 'respond_to_offer',
                 offerID_post : offerID
 				},
         success : function(response){
-			//jQuery('.respond_contents').html(response);
 
 		}
  	});
-/*
-		$.ajax({
-		 url: '/echo/html',
-		 data: {},
-		 success: function(){
-			 //window.open(plugins_url( 'readme.txt', __FILE__ ) );
-			 //BUG:  site_url is a PHP function!!
-			 window.open(ajax_object.site_url.concat("/respond-to-trade-offer/"));
-		 },
-		 async: true
-		});
-*/
 	  	window.open(ajax_object.site_url.concat("/respond-to-trade-offer/"));
 	});
 
